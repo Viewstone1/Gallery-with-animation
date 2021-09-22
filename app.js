dc2f25d18b0b9128fabfabca17511d980b5a5a8d
Ivan <86512502+Viewstone1@users.noreply.github.com>
2021-09-22 16:15:26 +0300

    modified function activeSlide

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const slides = document.querySelectorAll(".slide"); //через куэри получаем каждый из слайдов
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide"); //через куэри получаем каждый из слайдов
 
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
+  slides[activeSlide].classList.add("active");
 
-    slide.classList.add("active");
-  });
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      //первый способ работы с массивом через for, второй через forEach
+      slide.classList.remove("active");
+    });
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    //первый способ работы с массивом через for, второй через forEach
-    slide.classList.remove("active");
-  });
-}
+slidesPlugin(2);
